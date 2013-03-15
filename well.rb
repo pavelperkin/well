8ff8d2b11d44ab8a8be74d1a5f4f4d08eaf0338c
Pavel Perkin <pavel.perkin@anahoret.com>
2013-03-15 18:01:54 +0200

    smth went wrong =(

--- well.rb
+++ well.rb
@@ -1,7 +1,13 @@
 def well disks, rings, count
-  return count if rings.length == 0
-  return count if disks.length == 0
-  return count if disks[0] > rings[0]
+  p disks
+  p rings
+  return count if (rings.length == 0)
+  return count if (disks.length == 0)
+  return count if (disks[0] > rings[0])
+  return count if (disks[0] > rings[0]) and disks.length > 0
   disks = disks[0..disks.index(disks.max)] if disks.max > rings.min
   well(disks[1..-1], rings[0...(rings.index{ |x| x< disks[0]} || -1)], count+1)
 end
+
+f = File.open('in.txt')
+p well f.first.split, f.first.split, 0 # print disks count
