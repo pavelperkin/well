023bfd0544022a8e0c37cffc0876ebf6df8ca1b8
Pavel Perkin <pavel.perkin@anahoret.com>
2013-03-18 13:49:21 +0200

    need fix again

--- well.rb
+++ well.rb
@@ -4,10 +4,11 @@ def well disks, rings, count
   return count if (rings.length == 0)
   return count if (disks.length == 0)
   return count if (disks[0] > rings[0])
-  return count if (disks[0] > rings[0]) and disks.length > 0
-  disks = disks[0..disks.index(disks.max)] if disks.max > rings.min
-  well(disks[1..-1], rings[0...(rings.index{ |x| x< disks[0]} || -1)], count+1)
+  return count if (disks[0] > rings[1].to_i) and (disks.length == 0)
+  rings = rings[0...rings.index(rings.min)] if disks.min > rings.min
+  disks = disks[0..(disks.count(disks.max) > 1 ? disks.length - disks.reverse.index(disks.max) - 1 : disks.index(disks.max))] if disks.max > rings.min
+  well(disks[1..-1], rings[0...(rings.index{ |x| x < disks[0]} || -1)], count+1)
 end
 
 f = File.open('in.txt')
-p well f.first.split, f.first.split, 0 # print disks count
+p well f.first.split.map{|x| x.to_i}, f.first.split.map{|x| x.to_i}, 0 # print disks count
